94c4b81561fce0f2296faecd32aa20a7c40e9ef9
James <marshjfm@amazon.com>
2024-12-01 16:48:34 +0000

    improved p2

--- AOC-2024/day1/part2.py
+++ AOC-2024/day1/part2.py
@@ -1,21 +1,27 @@
+from collections import Counter
+import time 
+
 def main(lines):
     first_numbers = []
     second_numbers = []
     total_score= 0
+    
     for line in lines: 
         n1,n2 = line.split('   ')
         n1,n2 = int(n1),int(n2)
         first_numbers.append(n1)
         second_numbers.append(n2)
-    
-    for i in range(len(lines)):
-        score = first_numbers[i] * second_numbers.count(first_numbers[i])
-        total_score += score
+
+    second_counts = Counter(second_numbers)
+
+    for number in first_numbers:   
+        total_score += second_counts[number] * number
     
     print(total_score)
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
-        lines = f.read().splitlines()
+        lines = f.read().splitlines()      
     main(lines)
+    
     
